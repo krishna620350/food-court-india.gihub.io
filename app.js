84e7e8235303e9c0c670eebb4a327422a0c4d47e
krishna <manish620350@gmail.com>
2021-01-29 13:42:29 +0530

    second request
    my idea

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ $(document).ready(function(){
         slidesToShow:3,
         slidesToScroll:1,
         prevArrow:".prev-btn",
-        nextArrow:".next-btn"
+        nextArrow:".next-btn",
+
+        responsive:[
+            {
+                breakpoint:992,
+                settings:{
+                    slidesToShow:2
+                }
+            },
+            {
+                breakpoint:768,
+                settings:{
+                    slidesToShow:1
+                }
+            }
+        ]
     });
+    $('.nav-triger').click(function(){
+        $('.sit-contant-wreper').toggleClass('scaled');
+    })
 });
